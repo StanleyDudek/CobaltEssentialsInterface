392ddb6ea7222c88bf9dce7e7336527c673a95ce
Dudekahedron <49531350+StanleyDudek@users.noreply.github.com>
2022-03-21 16:23:00 -0400

    improve time play sync for slower computers

---
+++ Resources/Client/CEI/lua/vehicle/extensions/CEI/CEIPhysics.lua
@@ -0,0 +1,36 @@
+
+local M = {}
+
+local counter = 0
+local physstart = 0
+
+local physmult = 1
+
+local physHandlerAdded = false
+
+local function update(dtSim)
+	if counter == 0 then 
+		physstart = os.clock()
+	end
+	counter = counter + 1
+	if counter == 2000 then
+		counter = 0
+		local physend = os.clock()
+		local physdiff = physend - physstart
+		physmult = 1 / physdiff
+		--print(tostring(physmult*100) .. "% realtime")
+		obj:queueGameEngineLua("CEI.setPhysicsSpeed("..physmult..")")
+	end
+end
+
+local function updateGFX(dt)
+	if not physHandlerAdded and MPVehicleVE then
+		MPVehicleVE.AddPhysUpdateHandler('CEIPhysics', M.update)
+		physHandlerAdded = true
+	end
+end
+
+M.update = update
+M.updateGFX = updateGFX
+
+return M
