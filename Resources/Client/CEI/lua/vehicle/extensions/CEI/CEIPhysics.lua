2d7b477cb8fcbcb6f27189708fa24b5cec7246fc
Dudekahedron <49531350+StanleyDudek@users.noreply.github.com>
2022-09-21 10:17:23 -0400

    for 0.26, AddPhysUpdateHandler removed
    not sure if this breaks anything like time sync, but this does fix CEI breaking vehicles

--- Resources/Client/CEI/lua/vehicle/extensions/CEI/CEIPhysics.lua
+++ Resources/Client/CEI/lua/vehicle/extensions/CEI/CEIPhysics.lua
@@ -6,13 +6,8 @@ local physstart = 0
 
 local physmult = 1
 
-local physHandlerAdded = false
-
 local function update(dtSim)
-	if not playerInfo.firstPlayerSeated then
-		return
-	end
-	if counter == 0 then 
+	if counter == 0 then
 		physstart = os.clock()
 	end
 	counter = counter + 1
@@ -20,19 +15,13 @@ local function update(dtSim)
 		counter = 0
 		local physend = os.clock()
 		local physdiff = physend - physstart
-		physmult = 1 / physdiff
+		if playerInfo.firstPlayerSeated then
+			physmult = 1 / physdiff
 		obj:queueGameEngineLua("CEI.setPhysicsSpeed("..physmult..")")
-	end
-end
-
-local function updateGFX(dt)
-	if not physHandlerAdded and MPVehicleVE then
-		MPVehicleVE.AddPhysUpdateHandler('CEIPhysics', M.update)
-		physHandlerAdded = true
+		end
 	end
 end
 
 M.update = update
-M.updateGFX = updateGFX
 
 return M
