491985703c6e1d1a86882bd1f91b53d034756059
Dudekahedron <49531350+StanleyDudek@users.noreply.github.com>
2024-07-24 12:06:03 -0400

    version bump

--- Resources/Client/CEI/lua/ge/extensions/CEI.lua
+++ Resources/Client/CEI/lua/ge/extensions/CEI.lua
@@ -2,7 +2,7 @@
 
 local M = {}
 
-local CEI_VERSION = "0.7.100"
+local CEI_VERSION = "0.7.101"
 local logTag = "CEI"
 local gui_module = require("ge/extensions/editor/api/gui")
 local gui = {setupEditorGuiTheme = nop}
